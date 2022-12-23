4626e8e33b6a559707d94ca98f04697de945d604
lvvcho <lsilvaunap@gmail.com>
2022-12-22 22:05:11 -0300

    update

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+#Django busca urls de arriba hacia abajo hasta que coincida con alguno de ellos
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blogs.urls')) #se dirige a los urls de blogs
 ]
