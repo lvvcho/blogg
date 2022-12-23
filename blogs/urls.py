d07986850d6b5898bac77f59db8768785f46d454
lvvcho <lsilvaunap@gmail.com>
2022-12-22 22:52:17 -0300

    update diseño

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from blogs import views
+
+app_name='blogs'
+
 urlpatterns = [
-    path('', views.home_page)
+    path('', views.home_page),
+    path('post/<int:pk>', views.PostDetailView.as_view(), name='post')
 ]
+
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
+from blogs.models import Post, Category #vamos importando nuestros modelos
 
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse('Hola')
+    #Con esto obtenemos todos los post y categorias
+    posts=Post.objects.all()
+    categories=Category.objects.all()
+    featured=Post.objects.filter(featured=True) [:3]
+
+    context={
+        'post_list':posts,
+        'categories':categories,
+        'featured':featured
+    }
+
+    return render(request, 'blogs/home_page.html', context=context)
+
+
+
+class PostDetailView(generic.DetailView):
+    model=Post
